a1e0b05124b874774fc4e6fccea3ddb12d6f2ee9
kaiqueoleme <166898993+kaiqueoleme@users.noreply.github.com>
2025-03-10 02:46:25 -0300

    Arrumando dimensões da janela do app

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -28,5 +28,13 @@ namespace MauiAppMinhasCompras
             //MainPage = new AppShell();
             MainPage = new NavigationPage(new Views.ListaProduto());
         }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Width = 400;
+            window.Height = 600;
+            return window;
+        }
     }
 }

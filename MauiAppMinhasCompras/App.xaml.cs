7b1c9885f9802041a93d87301bf1a24d778a4e2d
kaiqueoleme <166898993+kaiqueoleme@users.noreply.github.com>
2025-03-13 04:24:42 -0300

    Criação da SearchBar e ListView, implementação dos eventos de Somar, TextChanged e OnAppearing

--- MauiAppMinhasCompras/App.xaml.cs
+++ MauiAppMinhasCompras/App.xaml.cs
@@ -32,8 +32,8 @@ namespace MauiAppMinhasCompras
         protected override Window CreateWindow(IActivationState? activationState)
         {
             var window = base.CreateWindow(activationState);
-            window.Width = 400;
-            window.Height = 600;
+            window.Width = 440;
+            window.Height = 640;
             return window;
         }
     }

84b62f4b3ce198459877a86ae4357d77772ce1d3
kaiqueoleme <166898993+kaiqueoleme@users.noreply.github.com>
2025-03-21 01:57:28 -0300

    Validação da inserção de produtos

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -13,12 +13,24 @@ public partial class NovoProduto : ContentPage
     {
 		try
 		{
+			if (txt_quantidade.Text == "")
+			{
+				txt_quantidade.Text = "0";
+
+            }
+
+			if (txt_preco.Text == "")
+			{
+                txt_preco.Text = "0";
+            }
+
 			Produto p = new Produto
 			{
 				Descricao = txt_descricao.Text,
 				Quantidade = Convert.ToDouble(txt_quantidade.Text),
 				Preco = Convert.ToDouble(txt_preco.Text)
 			};
+			
 
 			await App.Db.Insert(p);
 			await DisplayAlert("Sucesso", "Registro Inserido", "Ok");

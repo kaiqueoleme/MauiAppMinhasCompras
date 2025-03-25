d8d3a76483971d638bd318eed680058ee3f4adc1
kaiqueoleme <166898993+kaiqueoleme@users.noreply.github.com>
2025-03-25 04:03:54 -0300

    Atualização interface e pull and refresh

--- MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
+++ MauiAppMinhasCompras/Views/NovoProduto.xaml.cs
@@ -25,6 +25,7 @@ public partial class NovoProduto : ContentPage
 
 			await App.Db.Insert(p);
 			await DisplayAlert("Sucesso", "Registro Inserido", "Ok");
+			await Navigation.PopAsync();
 
 		} catch (Exception ex)
 		{
